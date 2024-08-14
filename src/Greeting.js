5ef28bea3b61af5aad22df9b96c3e78a21663619
Gladstone Rose <gladstonerose@hotmail.com>
2024-08-13 19:33:37 -0500

    modify Greeting.js

--- src/Greeting.js
+++ src/Greeting.js
@@ -1,7 +1,8 @@
 import React from 'react';
 
 function Greeting() {
-  return <h1>Hello, welcome to my app!</h1>;
+  return <h1>Hello, welcome to my app! showing how to make an 
+App with React</h1>;
 }
 
 export default Greeting;
